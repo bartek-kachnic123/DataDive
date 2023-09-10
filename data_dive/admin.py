b46e0fcefbc6bb2453d2f76cfea95262355b5c4c
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-09-10 17:37:35 +0200

    register Page and Category models

--- data_dive/admin.py
+++ data_dive/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from data_dive import models
+from data_dive import admin_models
+
+
+
 # Register your models here.
+admin.site.register(models.Category, admin_models.CategoryAdmin)
+
+admin.site.register(models.Page, admin_models.PageAdmin)
