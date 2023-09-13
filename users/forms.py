92a339f5435ca18322f66c511e630085375a4066
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-09-13 09:31:20 +0200

    Added RegisterUserForm and UserProfileForm

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from users.models import UserProfile
+
+class RegisterUserForm(forms.UserCreationForm):
+    email = forms.EmailField(max_length=128, required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+
+        fields = ['website_url', 'picture']
