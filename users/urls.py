65cd79a347e0dc94b69c4952572c563ccc73a55a
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-09-15 11:55:13 +0200

    Added user_login view, login template, url updated

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from users.views import register
+from users.views import (
+    register,
+    user_login
+)
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', user_login, name='user_login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import login
 from users.forms import (
     RegisterUserForm,
-    UserProfileForm
+    UserProfileForm,
+    LoginUserForm
 )
+from users.utils import user_authenticate
 # Create your views here.
 
 
@@ -34,3 +37,25 @@ def register(request):
     }
 
     return render(request, 'users/register.html', context=context_dict)
+
+def user_login(request):
+    login_user_form = LoginUserForm()
+    if request.method == 'POST':
+        login_user_form = LoginUserForm(request.POST)
+
+        if login_user_form.is_valid():
+            user = user_authenticate(login_form=login_user_form)
+            
+            if user and user.is_active:
+                login(request, user)
+                return redirect(reverse('data_dive:index'))
+            elif user:
+                login_user_form.add_error('password', 'Account is disabled!')
+            else:
+                login_user_form.add_error('password', 'Invalid username, email or password')
+
+    context_dict = {
+        'title' : 'Login',
+        'login_user_form' : login_user_form
+    }
+    return render(request, 'users/login.html', context=context_dict)
