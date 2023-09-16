e86dac3559cc803578c0220c61031e71861d624f
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-09-16 19:30:52 +0200

    Added LOGIN_URL setting

--- data_dive_django_project/settings.py
+++ data_dive_django_project/settings.py
@@ -159,3 +159,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+# LOGIN SETTINGS
+
+LOGIN_URL = 'users:user_login'
