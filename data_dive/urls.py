e538703dc0b048765eb4debbefa2c712f6979442
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-10-01 12:41:46 +0200

    Added LikeCategoryView to urls

--- data_dive/urls.py
+++ data_dive/urls.py
@@ -4,15 +4,18 @@ from data_dive.views import (
     show_category,
     add_category,
     add_page,
-    goto_url
+    goto_url,
+    like_category_view
 )
 
 app_name = 'data_dive'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('category/<slug:category_name_slug>/', show_category, name='show_category'),
+    path('category/<slug:category_name_slug>/',
+         show_category, name='show_category'),
     path('add_category/', add_category, name='add_category'),
     path('add_page/', add_page, name='add_page'),
-    path('goto/', goto_url, name='goto_url')
+    path('goto/', goto_url, name='goto_url'),
+    path('like_category/', like_category_view, name='like_category')
 ]
